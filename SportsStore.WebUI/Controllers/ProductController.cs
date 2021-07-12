3898bac7df61d4857155ddc6b01eff3c7a0c7167
ken347 <dp27530@gmail.com>
2021-07-13 04:12:58 +0800

    完成產品分頁，及通過分頁測試

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -5,18 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository _repository;
+        public int pageSize = 4;
 
         public ProductController(IProductRepository repository) => _repository = repository;
 
-        public ViewResult List()
+        public ViewResult List(int page=1)
         {
-            return View(_repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel()
+            {
+                Products = _repository.Products.OrderBy(p => p.ProductID)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize),
+                PagingInfo = new PagingInfo()
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            };
+       
+            return View(model);
         }
+
+
     }
 }
