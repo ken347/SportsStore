9de27c04968f3010ebb1305121831dcafa944074
ken347 <dp27530@gmail.com>
2021-07-12 22:20:25 +0800

    注入模擬數據庫

--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -22,8 +25,30 @@ namespace SportsStore.WebUI.Infrastructure
         public IEnumerable<object> GetServices(Type serviceType) => kernel.GetAll(serviceType);
 
         private void AddBinding()
-        { 
-            // TODO 注入依賴關係
+        {
+            // TODO 之後替換實現
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>()
+            { 
+                new Product()
+                { 
+                    Name="Football",
+                    Price=25
+                },
+                new Product()
+                { 
+                    Name="Surf board",
+                    Price=179
+                },
+                new Product()
+                { 
+                    Name="Running shoes",
+                    Price=95
+                }
+            });
+
+            //ToConstant() 單例
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
