d0cc0918e069825c950e5060d980cf3e5e599a74
ken347 <dp27530@gmail.com>
2021-07-13 16:48:56 +0800

    修改及新增路由規則

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,16 +14,33 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             //路由具有順序性，由上至下
+            // TODO 為路由命名
 
             routes.MapRoute(
-                name:null,
-                url:"Page{page}",
-                defaults:new { controller="Product",action="List"});
+                name: null,
+                url: "",
+                defaults: new { controller = "Product", action = "List", category = (string)null, page = 1 });
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Product", action = "List" },
+                constraints: new { page = @"\d+" });
+
+            routes.MapRoute(
+                name: null,
+                url: "{category}",
+                defaults: new { controller = "Product", action = "List", page = 1 });
+
+            routes.MapRoute(
+                name: null,
+                url: "{category}/Page{page}",
+                defaults: new { controller = "Product", action = "List" },
+                constraints: new { page = @"\d+" });
+
+            routes.MapRoute(
+                name: null,
+                url: "{controller}/{action}}"
             );
         }
     }
