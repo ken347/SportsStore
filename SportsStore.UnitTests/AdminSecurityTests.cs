b299123f506e900aa96d09f97412c0bcaa30cc11
ken347 <dp27530@gmail.com>
2021-07-15 04:09:22 +0800

    新增登入功能並通過測試

---
+++ SportsStore.UnitTests/AdminSecurityTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            //準備
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+
+            LoginViewModel model = new LoginViewModel()
+            {
+                UserName = "admin",
+                Password = "secret"
+            };
+
+            AccountController target = new AccountController(mock.Object);
+
+            //動作
+            ActionResult result = target.Login(model,"/MyUrl");
+
+            //斷言
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyUrl",((RedirectResult)result).Url);
+        }
+
+        [TestMethod]
+        public void Cannot_Login_With_Invalid_Credentials()
+        {
+            //準備
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("badUser", "badPass")).Returns(false);
+
+            LoginViewModel model = new LoginViewModel()
+            {
+                UserName = "badUser",
+                Password = "badPass"
+            };
+
+            AccountController target = new AccountController(mock.Object);
+
+            //動作
+            ActionResult result = target.Login(model,"MyUrl");
+
+            //斷言
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthProvider _authProvider;
+
+        public AccountController(IAuthProvider authProvider) => _authProvider = authProvider;   
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model,string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (_authProvider.Authenticate(model.UserName, model.Password))
+            {
+                return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+            }
+
+            ModelState.AddModelError("","帳號或密碼錯誤");
+            return View();
+        }
+    }
+}
