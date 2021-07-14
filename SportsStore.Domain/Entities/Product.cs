42c02089a37304bab9f602a24f909fb5db2e3e2e
ken347 <dp27530@gmail.com>
2021-07-15 00:02:53 +0800

    完成編輯產品的視圖

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -4,27 +4,34 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SportsStore.Domain.Entities
 {
     public class Product
     {
         [Key]
+        [HiddenInput(DisplayValue=false)]
         public int ProductID { get; set; }
 
         [Required]
         [MaxLength(100)]
+        [Display(Name="名稱")]
         public string Name { get; set; }
 
         [Required]
         [MaxLength(500)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "描述")]
         public string Description { get; set; }
 
         [Required]
+        [Display(Name = "單價")]
         public decimal Price { get; set; }
 
         [Required]
         [MaxLength(50)]
+        [Display(Name = "分類")]
         public string Catagory { get; set; }
     }
 }
--- SportsStore.UnitTests/AdminTests.cs
+++ SportsStore.UnitTests/AdminTests.cs
@@ -47,5 +47,75 @@ namespace SportsStore.UnitTests
             Assert.AreEqual("P2",result[1].Name);
             Assert.AreEqual("P3", result[2].Name);
         }
+
+        [TestMethod]
+        public void Can_Edit_Product()
+        {
+            //準備
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product()
+                    {
+                        ProductID=1,
+                        Name="P1"
+                    },
+                    new Product()
+                    {
+                        ProductID=2,
+                        Name="P2"
+                    },
+                    new Product()
+                    {
+                        ProductID=3,
+                        Name="P3"
+                    }
+                });
+
+            AdminController target = new AdminController(mock.Object);
+
+            //動作
+            Product p1 = target.Edit(1).ViewData.Model as Product;
+            Product p2 = target.Edit(2).ViewData.Model as Product;
+            Product p3 = target.Edit(3).ViewData.Model as Product;
+
+            //斷言
+            Assert.AreEqual(1,p1.ProductID);
+            Assert.AreEqual(2,p2.ProductID);
+            Assert.AreEqual(3, p3.ProductID);
+        }
+
+        [TestMethod]
+        public void Cannot_Edit_Nonexistent_Product()
+        {
+            //準備
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product()
+                    {
+                        ProductID=1,
+                        Name="P1"
+                    },
+                    new Product()
+                    {
+                        ProductID=2,
+                        Name="P2"
+                    },
+                    new Product()
+                    {
+                        ProductID=3,
+                        Name="P3"
+                    }
+                });
+
+            AdminController target = new AdminController(mock.Object);
+
+            //動作
+            Product result = (Product)target.Edit(4).ViewData.Model;
+
+            //斷言
+            Assert.IsNull(result);
+        }
     }
 }
