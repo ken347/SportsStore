1f95f819476fc346c931879cd3d519004008b372
ken347 <dp27530@gmail.com>
2021-07-14 23:29:38 +0800

    完成管理者產品列表顯示功能

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository _repository;
+
+        public AdminController(IProductRepository repository) => _repository = repository;
+
+        public ViewResult Index()
+        {
+            return View(_repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = _repository.Products.FirstOrDefault(p=>p.ProductID==productId);
+
+            return View(product);
+        }
+    }
+}
